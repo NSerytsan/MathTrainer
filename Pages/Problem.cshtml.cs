2a15de2f9192541b175cb6b32187a01eddcc1f7e
NSerytsan <nserytsan@gmail.com>
2022-10-10 20:12:55 +0300

    Add project files.

---
+++ Pages/Problem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MathTrainer.Pages
+{
+    public class ProblemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/problem_script.js
@@ -0,0 +1,23 @@
+﻿var p_left = document.getElementById("problem-number").value;
+var checked = false;
+function start() {
+    alert("Починаймо!");
+    window.open("https://localhost:7228/Problem", "_parent");
+}
+function check_answer(r) {
+    checked = false;
+    var num = document.getElementById("answer").value;
+    var res = r;
+
+    if (num == res) {
+        alert("Вірно");
+        checked = true;
+    } else {
+        alert("Помилка");
+        checked = true;
+    }
+
+    if (checked == true && p_left > 0) {
+        window.open("https://localhost:7228/Problem", "_parent");
+    }
+}
