1aa45f8d17c86e85c96ad815e95eeac42e36ead5
NSerytsan <nserytsan@gmail.com>
2023-03-10 13:52:37 +0200

    multiplication

---
+++ Pages/ProblemMult.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MathTrainer.Pages
+{
+    public class ProblemMultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
